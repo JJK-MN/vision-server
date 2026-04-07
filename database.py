cb7a0c088319a32c1f2e1497561e83239fc32997
_ProtonPLUS <mcib088@aucklanduni.ac.nz>
2026-04-07 13:16:06 +1200

    Started working on the database endpoints on the RESTAPI, implemented create user and authenticate user methods with hashing

---
+++ database.py
@@ -0,0 +1,38 @@
+import os
+import bcrypt
+from supabase import create_client, Client
+
+url: str = str(os.environ.get("SUPABASE_URL"))
+key: str = str(os.environ.get("SUPABASE_KEY"))
+supabase: Client = create_client(url, key)
+
+# Instead of generating a new hash for each failed authentication attempt, we can use a fixed dummy hash.
+# This way, we can ensure that the time taken for authentication is consistent regardless of whether the user exists or not, which helps mitigate timing attacks.
+DUMMY_HASH = b"$2b$12$R9h/cIPz0gi.URNNX3kh2UXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+def create_user(username: str, password: str):
+    existing_user = supabase.table("USERS").select("*").eq("username", username).execute()
+
+    if existing_user.data:
+        raise ValueError("User already exists")
+
+    password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    supabase.table("USERS").insert({
+        "username": username,
+        "password": password_hash.decode("utf-8")  # store as string
+    }).execute()
+
+def authenticate_user(username: str, password: str) -> bool:
+    user = supabase.table("USERS").select("password").eq("username", username).execute()
+    data : list = user.data
+
+    if len(data) == 0:
+        bcrypt.checkpw(password.encode(), DUMMY_HASH)
+        return False
+    
+    stored_password = data[0].get("password")
+    if not isinstance(stored_password, str):
+        return False
+
+    return bcrypt.checkpw(password.encode(), stored_password.encode())
