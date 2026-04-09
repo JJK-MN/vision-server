eac2a56fa87ef2515081483a47e9b43f7a9fa679
_ProtonPLUS <mcib088@aucklanduni.ac.nz>
2026-04-09 21:38:27 +1200

    Created the nessesary endpoints for voice and question answering

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import tempfile
+from voice_processing import transcribe_audio
+from model import generate_response
+from flask import Flask, jsonify, request
 import database
 #
 #                MAIN FILE FOR SERVER
@@ -25,7 +28,58 @@ def create_user(username: str, password: str):
     except ValueError as e:
         return {"error": str(e)}, 400
 
+# Method to send a auth token to the client for authentication
+@app.route("/login/<username>/<password>", methods=["POST"])
+def login(username: str, password: str):
+    try:
+        token = database.authenticate_user(username, password)
+        return {"token": token}, 200
+    except ValueError as e:
+        return {"error": str(e)}, 401
 
+@app.route("/transcribe/<token>", methods=["POST"])
+def transcribe(token: str):
+    if "file" not in request.files:
+        return {"error": "No file provided"}, 400
+    
+    if database.verify_auth_token(token) is None:
+        return {"error": "Invalid or expired token"}, 401
+    
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "Empty filename"}), 400
+    
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".wav") as tmp:
+        file.save(tmp.name)
+
+        result = transcribe_audio(tmp.name)
+
+    return jsonify({
+        "text": result["text"] # type: ignore
+    })
+
+@app.route("/ask/<token>/<question>", methods=["POST"])
+def ask(token: str, question: str):
+    if database.verify_auth_token(token) is None:
+        return {"error": "Invalid or expired token"}, 401
+    
+    if "file" not in request.files:
+        return {"error": "No file provided"}, 400
+    
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "Empty filename"}), 400
+    
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".wav") as tmp:
+        file.save(tmp.name)
+
+    generate_response(tmp.name, question)
+
+    return jsonify({
+        "response": generate_response(tmp.name, question)
+    })
 
 if __name__ == "__main__":
     app.run()
--- voice_processing.py
+++ voice_processing.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 import torch
 
-pipe = pipeline(
+VoicePipeline = pipeline(
     "automatic-speech-recognition",
     model="distil-whisper/distil-large-v3.5",
     device=0,  # use GPU
@@ -9,9 +9,9 @@ pipe = pipeline(
 )
 
 def transcribe_audio(audio_path: str) -> str:
-    result = pipe(
+    result = VoicePipeline(
         audio_path,
         chunk_length_s=30,   # important for long audio
         batch_size=8         # tweak based on VRAM
     )
-    return result["text"]
+    return result["text"] # type: ignore
